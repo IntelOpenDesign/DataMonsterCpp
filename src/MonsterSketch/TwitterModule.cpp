1cda1f5d84be3d43bbaaa47f53d8d4883ce8f863
Carlos Montesinos <carlos.montesinos@intel.com>
2013-09-19 11:52:37 -0700

    Added empty TwitterModule.

---
+++ src/MonsterSketch/TwitterModule.cpp
@@ -0,0 +1,42 @@
+#ifndef TWITTER_MODULE_CPP
+#define TWITTER_MODULE_CPP
+
+#include "Arduino.h"
+
+class TwitterModule{
+
+  // Member
+public:
+ 
+
+private:
+
+// Methods
+
+public:
+  //Constructor
+  TwitterModule(){
+
+
+  }
+
+void update()
+{
+  
+}
+
+bool gotTweet()
+{
+  
+  return false;
+}
+
+};
+
+#endif // TWITTER_MODULE_CPP
+
+
+
+
+
+
